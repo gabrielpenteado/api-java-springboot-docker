c62a4809b496a2e039cbd875c3c0a5be4b25db79
Gabriel Penteado <gabripenteado@gmail.com>
2024-02-18 15:21:59 -0300

    start project and config docker database

---
+++ src/test/java/com/gabrielpenteado/apijavaspringbootdocker/ApiJavaSpringbootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielpenteado.apijavaspringbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaSpringbootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
