2085797a78f77d64d04c32a2d0167eb3a45b2b28
Gabriel Penteado <gabripenteado@gmail.com>
2024-02-18 15:21:59 -0300

    start project and config docker database

---
+++ src/main/java/com/gabrielpenteado/apijavaspringbootdocker/ApiJavaSpringbootDockerApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielpenteado.apijavaspringbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaSpringbootDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaSpringbootDockerApplication.class, args);
+	}
+
+}
