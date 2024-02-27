74c247b4aec5d57d703372ff8e830784d061f04f
Gabriel Penteado <gabripenteado@gmail.com>
2024-02-27 00:54:22 -0300

    created LICENSE.md and README.md

--- src/main/java/com/gabrielpenteado/apijavaspringbootdocker/controllers/UserController.java
+++ src/main/java/com/gabrielpenteado/apijavaspringbootdocker/controllers/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.gabrielpenteado.apijavaspringbootdocker.services.UserService;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @RequestMapping("/users")
@@ -30,14 +32,25 @@ public class UserController {
     }
 
     @GetMapping("user")
-    public UserModel getUserController(@RequestParam String email) {
-        return userService.getUserService(email);
+    public UserModel getUserByEmailController(@RequestParam String email) {
+        return userService.getUserByEmailService(email);
+    }
+
+    @GetMapping("user/{id}")
+    public UserModel getUserByIdController(@PathVariable("id") UUID id) {
+        return userService.getUserByIdService(id);
     }
 
     @PostMapping("/add")
     public UserModel saveUserController(@RequestBody UserModel userModel) {
         UserModel userCreated = userService.saveUserService(userModel);
         return userCreated;
+    }
+
+    @PutMapping("update/{id}")
+    public UserModel updateUserController(@PathVariable UUID id, @RequestBody UserModel user) {
+        UserModel userUpdated = userService.updateUserService(id, user);
+        return userUpdated;
     }
 
     @DeleteMapping("deletebyid/{id}")
