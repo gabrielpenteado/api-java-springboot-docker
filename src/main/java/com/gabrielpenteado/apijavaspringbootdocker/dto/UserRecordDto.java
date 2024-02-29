8870dc8a94f7b9f61363e9272ef512506855bbcd
Gabriel Penteado <gabripenteado@gmail.com>
2024-02-29 00:40:14 -0300

    restful api with records, dto, responseentity

---
+++ src/main/java/com/gabrielpenteado/apijavaspringbootdocker/dto/UserRecordDto.java
@@ -0,0 +1,15 @@
+package com.gabrielpenteado.apijavaspringbootdocker.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record UserRecordDto(
+        @NotBlank @NotNull String name,
+
+        @NotBlank @NotNull String email,
+
+        @NotBlank @NotNull String address,
+
+        @NotBlank @NotNull String urlAvatar) {
+
+}
--- src/main/java/com/gabrielpenteado/apijavaspringbootdocker/repositories/UserRepository.java
+++ src/main/java/com/gabrielpenteado/apijavaspringbootdocker/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package com.gabrielpenteado.apijavaspringbootdocker.repositories;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,5 +10,5 @@ import com.gabrielpenteado.apijavaspringbootdocker.models.UserModel;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, UUID> {
-    UserModel findByEmail(String email);
+    Optional<UserModel> findByEmail(String email);
 }
